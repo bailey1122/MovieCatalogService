ea17014586a43e2cc628cc7498213a76ce750865
bailey1122 <illyashkurko112@gmail.com>
2020-12-30 16:40:36 +0200

    Initial commit: MovieCatalogService

--- src/main/java/com/pr/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/pr/moviecatalogservice/resources/MovieCatalogResource.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -29,12 +28,19 @@ public class MovieCatalogResource {
 //    private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
-    // Hystrix creates a proxy class which is wrapper containing the circuit
-    // breaker logic and return the response to a caller
-    // it detects that a service is down. Hystrix wraps a class in a proxy class
-    // and it returns a proxy class that Hystrix's created and wrapped around it
-    // if the circuit breaks, then don't call getCatalog. Call getFallbackCatalog instead
-    @HystrixCommand(fallbackMethod = "getFallbackCatalog") // the method which shouldn't cause the whole thing to go down
+//    // the method on the proxy is called first. In the case when the service is down, the
+//    // fallback is called. Hystrix doesn't have an opportunity to intersect two methods that
+//    // are called in the API class in the case when one method is calling another one in the same class.
+//    // Hystrix can intercept a call if an external class's method is calling the method but not
+      // a method which is calling the method inside the class. Therefore, we can solve the problem
+      // by taking the method out into another class, bean, or whatever. And we could have the main API
+      // method calling not another method of the same class but the method of another instance.
+//    // Hystrix creates a proxy class which is wrapper containing the circuit
+//    // breaker logic and return the response to a caller
+//    // it detects that a service is down. Hystrix wraps a class in a proxy class
+//    // and it returns a proxy class that Hystrix's created and wrapped around it
+//    // if the circuit breaks, then don't call getCatalog. Call getFallbackCatalog instead
+//    @HystrixCommand(fallbackMethod = "getFallbackCatalog") // the method which shouldn't cause the whole thing to go down
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
         // create an instance of the class, populate the properties to it, and get a fully form object
@@ -47,7 +53,7 @@ public class MovieCatalogResource {
         UserRating ratings = getUserRating(userId);
 
         // replace each rating with the catalog.
-        return ratings.getUserRating().stream().map(rating -> {
+        return ratings.getRatings().stream().map(rating -> {
 //            for every rated movie, take the movie's ID and call the movie information API with that ID
 //            Movie movie = restTemplate.getForObject("http://localhost:8092/movies/" + rating.getMovieId(), Movie.class);
 //            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
@@ -80,8 +86,8 @@ public class MovieCatalogResource {
         return new CatalogItem(movie.getName(), "Desc", rating.getRating());
     }
 
-    private CatalogItem getCatalogItem(Rating rating) {
-        
+    private CatalogItem getFallbackCatalogItem(Rating rating) {
+        return new CatalogItem("Movie name not found", "", rating.getRating());
     }
 
     @HystrixCommand(fallbackMethod = "getFallbackUserRating")
@@ -90,7 +96,13 @@ public class MovieCatalogResource {
     }
 
     private UserRating getFallbackUserRating(@PathVariable("userId") String userId) {
-
+        UserRating userRating = new UserRating();
+        userRating.setUserId(userId);
+        userRating.setRatings(Arrays.asList(
+                new Rating("0", 0)
+        ));
+        return userRating;
+    }
 
     // reduce the possibility of an error when a fallback method executes. Simple hard-coded verison
     public List<CatalogItem> getFallbackCatalog(@PathVariable String userId) {
